7ca33937cc02480855fecda0b59aea1ed5ac8b24
Milos Rankovic <mrankovic310@gmail.com>
2023-07-31 20:34:45 +0200

    Added prototype pattern

---
+++ prototype-pattern/practice/app.js
@@ -0,0 +1,31 @@
+/*
+    Prototype Coding Exercise
+
+    Given the definitions shown in code, you are asked to implement Line.deepCopy() 
+    to perform a deep copy of the given Line  object. This method should return a copy of a 
+    Line that contains copies of its start/end points.
+
+    TASK SETUP:
+
+    class Point {
+        constructor(x, y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class Line {
+
+        constructor(start, end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        deepCopy()
+        {
+            // todo
+        }
+    }
+*/
