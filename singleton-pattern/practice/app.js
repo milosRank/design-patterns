81c39b6108512aeeff23083d2d9a2ab1812d4617
Milos Rankovic <milos.rankovic@eximius-solutions.com>
2023-08-02 15:18:00 +0200

    Singleton pattern practice done

--- singleton-pattern/practice/app.js
+++ singleton-pattern/practice/app.js
@@ -10,9 +10,81 @@
 
     class SingletonTester {
 
-        static isSingleton(generator)
-        {
+        static isSingleton(generator) {
             // todo
         }
     }
 */
+
+
+class SingletonTester {
+
+
+    /**
+     * Check if function is returning a singleton
+     * 
+     * @param {Function} generator 
+     * 
+     * @returns {Boolean} - true if function result is singleton, false if not
+     */
+    static isSingleton(generator) {
+        
+        // Create temp instances
+        let firstInstance = generator("John", 33);
+        let secondInstance = generator("John", 33);
+
+        // Check if two instances are the same, not only by reference
+        let isInstanceSingleton = firstInstance === secondInstance ? true : false;
+
+        firstInstance = secondInstance = null; // Delete temporarly created instances
+
+        return isInstanceSingleton;
+    }
+}
+
+
+
+class EmployeeFactory {
+    
+    static createEmployee(name, age) {
+        return new Employee(name, age);
+    }
+}
+
+class Employee {
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+
+        // Make class singleton
+        const instance = this.constructor.instance;
+        if (instance) {
+            return instance;
+        }
+
+        this.constructor.instance = this;
+    }
+}
+
+class CarFactory {
+    
+    static createCar(brand, yearOfProduction) {
+        return new Car(brand, yearOfProduction);
+    }
+}
+
+class Car {
+
+    constructor(brand, yearOfProduction) {
+        this.brand = brand;
+        this.yearOfProduction = yearOfProduction;
+    }
+}
+
+
+let isEmployeeSingleton = SingletonTester.isSingleton(EmployeeFactory.createEmployee);
+let isCarSingleton = SingletonTester.isSingleton(CarFactory.createCar);
+
+console.log(isEmployeeSingleton); // true
+console.log(isCarSingleton); // false
